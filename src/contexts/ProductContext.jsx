20def1c8fe2c68bfb1f8143bb3d967f357fcb229
Ben Hur <benhurk2801@gmail.com>
2024-06-26 13:26:39 -0300

    Fetching the new api

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -6,7 +6,7 @@ export const ProductProvider = ({ children }) => {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:4000/products')
+        fetch('https://whiskylogged-api.vercel.app/products')
             .then(res => res.json())
             .then(catalog => {  
                 setProducts(catalog.sort((a, b) => ((b.rating.nose + b.rating.palate + b.rating.cost) / 3) - ((a.rating.nose + a.rating.palate + a.rating.cost) / 3))); 
