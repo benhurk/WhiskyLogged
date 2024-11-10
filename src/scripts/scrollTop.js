3381dbf7a08d345e1abe6e75116c1b8b97bf6f12
Ben Hur <benhurk2801@gmail.com>
2024-11-10 14:58:26 -0300

    Removida propriedade desnecessária e uso indevido de useEffect

--- src/scripts/scrollTop.js
+++ src/scripts/scrollTop.js
@@ -1,8 +1,4 @@
-import { useEffect } from "react"
-
-export const scrollTop = (currentProduct) => {
-    useEffect(() => {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    }, [currentProduct]);
+export const scrollTop = () => {
+    document.body.scrollTo({top: 0, behavior: 'smooth'}); // For Safari
+    document.documentElement.scrollTo({top: 0, behavior: 'smooth'}); // For Chrome, Firefox, IE and Opera
 }
